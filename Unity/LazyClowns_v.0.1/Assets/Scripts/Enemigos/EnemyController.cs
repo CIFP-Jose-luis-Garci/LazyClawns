324233111aec7569319c20302e0da45a4aaa6d05
Jairo Piña Salas <jairo.ps.gdm@gmail.com>
2022-01-08 21:38:46 +0100

    Ataque a Meele
    Pendiente de hacer el animator una vez que las animaciones estén pero el combate ya es funcional

---
+++ Unity/LazyClowns_v.0.1/Assets/Scripts/Enemigos/EnemyController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public int maxHealth = 100;
+
+    private int currentHealth = 100;
+    //private Animator animator;
+
+    private void Start()
+    {
+        //animator = GetComponent<Animator>();
+        currentHealth = 100;
+    }
+    private void Update()
+    {
+        print(currentHealth);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        //animator.SetTrigger("hit");
+        currentHealth -= damage;
+        
+        if (currentHealth <= 0)
+        {
+            EnemyDie();
+            print("MUERTO");
+        }
+    }
+
+    private void EnemyDie()
+    {
+       //animator.SetBool("dead", true);
+
+        GetComponent<Collider2D>().enabled = false;
+        this.enabled = false;
+    }
+}
---
+++ Unity/LazyClowns_v.0.1/Assets/Scripts/Player/Combat.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Combat : MonoBehaviour
+{
+   // private Animator animator;
+    [SerializeField] Transform puntoDeAtaque;
+    public float rangoAtaque = 0.5f;
+    public LayerMask enemyLayers;
+    public int daño = 30;
+
+
+
+    private void Awake()
+    {
+        //animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Q) /*&& siguienteAtaque >= tiempoAtaque*/)
+        {
+            Attack();
+            
+            print("Atacado");
+        }
+    }
+
+    private void Attack()
+    {
+        //animator.SetTrigger("attack");
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(puntoDeAtaque.position, rangoAtaque, enemyLayers);
+
+        foreach (Collider2D enemy in hitEnemies)
+        {
+           enemy.GetComponent<EnemyController>().TakeDamage(daño);
+        }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        if (puntoDeAtaque == null) return;
+        Gizmos.DrawWireSphere(puntoDeAtaque.position, rangoAtaque);
+
+    }
+}
