a6cab7d50c11e86130311e0301aa2a279700238a
vilkiin <jorgeeldiaz08@gmail.com>
2022-01-29 21:13:20 +0100

    Volumen en configuracion funcional
    añadir musica nuestra al juego y poner en las escenas
    (error que no se a que se refiere pero funciona igual)

--- Unity/LazyClowns_v.0.1/Assets/Scripts/Menus/Botones.cs
+++ Unity/LazyClowns_v.0.1/Assets/Scripts/Menus/Botones.cs
@@ -6,10 +6,18 @@ using UnityEngine.SceneManagement;
 
 public class Botones : MonoBehaviour
 {
+
+    [SerializeField] Text volumenText;
+    public float volumen;
+    [SerializeField] Slider mySlider;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mySlider.value = PlayerPrefs.GetFloat("volumenAudio", 0.5f);
+        AudioListener.volume = mySlider.value;
+
     }
 
     // Update is called once per frame
@@ -18,11 +26,22 @@ public class Botones : MonoBehaviour
         
     }
 
-   
+   public void Volumen(float valor)
+    {
+        volumen = valor;
+        PlayerPrefs.SetFloat("volumenAudio", volumen);
+        AudioListener.volume = mySlider.value;
+    }
+
+
     public void CargarEscena(int escena)
     {
         SceneManager.LoadScene(escena);
     }
 
-
+    public void SubirVolumen()
+    {
+        volumen = (mySlider.value);
+        volumenText.text = "VOLUMEN:" + volumen;
+    }
 }
