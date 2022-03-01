c2a9d3a7f57249449a27ff2f9dfa76e1656985e0
Jairo310 <jairo.ps.gdm@gmail.com>
2022-03-01 18:01:50 +0100

    Ajustes para builds

--- Unity/LazyClowns_v.0.1/Assets/Scripts/Player/PlayerMove.cs
+++ Unity/LazyClowns_v.0.1/Assets/Scripts/Player/PlayerMove.cs
@@ -109,7 +109,7 @@ public class PlayerMove : MonoBehaviour
         
         speed = 1;
         maxSpeed = 4f;
-        jumpForce = 4f;
+        jumpForce = 20f;
         distanciaSuelo = 0.25f;
         animator.SetBool("Muerto", false);
 
